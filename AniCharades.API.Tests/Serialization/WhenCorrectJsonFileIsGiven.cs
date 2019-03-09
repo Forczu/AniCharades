85e276d4e372612f4bb4763d90637f12db02d25c
Forczu <mateusz.forczmanski@gmail.com>
2019-03-10 00:23:52 +0100

    Added Unit Test project

---
+++ AniCharades.API.Tests/Serialization/WhenCorrectJsonFileIsGiven.cs
@@ -0,0 +1,38 @@
+﻿using AniCharades.API.Models;
+using AniCharades.API.Utils;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AniCharades.API.Tests.Serialization
+{
+    public class WhenCorrectJsonFileIsGiven
+    {
+        private IConfigurationRoot config;
+
+        public WhenCorrectJsonFileIsGiven()
+        {
+            var envVariable = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{envVariable}.json", optional: true)
+                    .Build();
+        }
+
+        [Fact]
+        public void SeriesCollectionShouldDeserializeCorrectly()
+        {
+            // given
+            string filePath = config["SeriesCollectionFile"];
+            string json = System.IO.File.ReadAllText(filePath);
+            // when
+            var seriesCollection = Utils.Serialization.JsonToEntityCollection<SeriesEntry>(json).ToArray();
+            // then
+            var accelWorldEntry = seriesCollection[3];
+            Assert.Equal("Accel World", accelWorldEntry.Title);
+        }
+    }
+}
