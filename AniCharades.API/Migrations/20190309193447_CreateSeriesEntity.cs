302db85cbcac9cc687d7a9eac147a45aae0625f7
Forczu <mateusz.forczmanski@gmail.com>
2019-03-09 20:40:22 +0100

    Migration for Series entity

---
+++ AniCharades.API/Migrations/20190309193447_CreateSeriesEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AniCharades.API.Migrations
+{
+    public partial class CreateSeriesEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    AnimePositions = table.Column<string>(nullable: true),
+                    MangaPositions = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
