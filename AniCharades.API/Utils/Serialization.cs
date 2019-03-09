1ff729988db0e2ade5ab654c6994ec50d319d476
Forczu <mateusz.forczmanski@gmail.com>
2019-03-09 23:55:07 +0100

    Added serialization helper functions

---
+++ AniCharades.API/Utils/Serialization.cs
@@ -0,0 +1,23 @@
+﻿using AniCharades.API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AniCharades.API.Utils
+{
+    public static class Serialization
+    {
+        /// <summary>
+        /// Helper function for converting data from JSON format to ORM entites.
+        /// </summary>
+        /// <typeparam name="T">Target entity type.</typeparam>
+        /// <param name="filePath"></param>
+        public static IEnumerable<T> JsonToEntityCollection<T>(string filePath)
+        {
+            var series = JsonConvert.DeserializeObject<IEnumerable<T>>(filePath);
+            return series;
+        }
+    }
+}
