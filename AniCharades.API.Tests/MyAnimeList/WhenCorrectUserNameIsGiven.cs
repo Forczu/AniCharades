18d485746177147888a63fc0f385b2e89ddf8413
Forczu <mateusz.forczmanski@gmail.com>
2019-03-14 16:26:17 +0100

    Added unit tests for getting resources through Jikan

---
+++ AniCharades.API.Tests/MyAnimeList/WhenCorrectUserNameIsGiven.cs
@@ -0,0 +1,32 @@
+﻿using AniCharades.API.Logic.Implementation;
+using AniCharades.API.Logic.Interfaces;
+using JikanDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AniCharades.API.Tests.MyAnimeList
+{
+    public class WhenCorrectUserNameIsGiven
+    {
+        IMyAnimeListService myAnimeListService;
+
+        public WhenCorrectUserNameIsGiven()
+        {
+            myAnimeListService = new MyAnimeListService(new Jikan(true));
+        }
+
+        [Fact]
+        public void ItShouldGetEntrieAnimeList()
+        {
+            // given
+            var username = "Ervelan";
+            // when
+            var animeList = myAnimeListService.GetAnimeList(username).Result;
+            // then
+            Assert.Contains(animeList, a => a.Title == "Yuuki Yuuna wa Yuusha de Aru");
+        }
+    }
+}
