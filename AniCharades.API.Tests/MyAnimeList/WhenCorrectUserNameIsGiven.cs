2bebb05b789c950b56f120aae29e2af9d8b18ed1
Forczu <mateusz.forczmanski@gmail.com>
2019-03-16 15:49:28 +0100

    Added a mocked Jikan animelist results

--- AniCharades.API.Tests/MyAnimeList/WhenCorrectUserNameIsGiven.cs
+++ AniCharades.API.Tests/MyAnimeList/WhenCorrectUserNameIsGiven.cs
@@ -1,8 +1,13 @@
 ﻿using AniCharades.API.Logic.Implementation;
 using AniCharades.API.Logic.Interfaces;
 using JikanDotNet;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Binder;
+using Moq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Xunit;
@@ -11,11 +16,38 @@ namespace AniCharades.API.Tests.MyAnimeList
 {
     public class WhenCorrectUserNameIsGiven
     {
-        IMyAnimeListService myAnimeListService;
+        private readonly IConfigurationRoot config;
+        private readonly IMyAnimeListService myAnimeListService;
 
         public WhenCorrectUserNameIsGiven()
         {
-            myAnimeListService = new MyAnimeListService(new Jikan(true));
+            var envVariable = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{envVariable}.json", optional: true)
+                    .Build();
+            var jikanMock = PrepareJikanMock();
+            myAnimeListService = new MyAnimeListService(jikanMock.Object);
+        }
+
+        private Mock<IJikan> PrepareJikanMock()
+        {
+            var ervieKey = "Jikan:User:Ervelan:Animelist";
+            var filePath = config[ervieKey + ":Path"];
+            var pageNubmer = Convert.ToInt32(config[ervieKey + ":PageNumber"]);
+            var animeListPageTemplate = config[ervieKey + ":PageNameTemplate"];
+            var filters = UserAnimeListExtension.All;
+
+            Mock<IJikan> jikanMock = new Mock<IJikan>();
+            for (int index = 1; index <= pageNubmer; index++)
+            {
+                var pageFilePath = string.Format("{0}\\{1}{2}.json", filePath, animeListPageTemplate, index);
+                var pageJson = File.ReadAllText(pageFilePath);
+                jikanMock.Setup(j => j.GetUserAnimeList("Ervelan", filters, index)).ReturnsAsync(
+                    JsonConvert.DeserializeObject<UserAnimeList>(pageJson)
+                );
+            }
+            return jikanMock;
         }
 
         [Fact]
