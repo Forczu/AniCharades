aeebe5af812bba3c65933eeeb049c3c63895f5ca
Forczu <mateusz.forczmanski@gmail.com>
2019-03-20 16:28:34 +0100

    Implemented partially anime & manga concrete assemblers

---
+++ AniCharades.API/Algorithms/SeriesAssembler/AnimeSeriesAssembler.cs
@@ -0,0 +1,43 @@
+﻿using AniCharades.API.Adapters.Jikan;
+using JikanDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AniCharades.API.Algorithms.SeriesAssembler
+{
+    public class AnimeSeriesAssembler : AbstractSeriesAssembler<JikanAnimeAdapter>
+    {
+        private static readonly int RetryMaxNumber = 3;
+        private static readonly int RetryWaitingTime = 60 * 1000;
+
+        private readonly IJikan jikan;
+
+        public AnimeSeriesAssembler(IJikan jikan)
+        {
+            this.jikan = jikan;
+        }
+
+        protected override bool CheckIfRelationIsValidForSeries(JikanAnimeAdapter sourceEntry, JikanAnimeAdapter relatedEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override JikanAnimeAdapter GetEntry(long entryId)
+        {
+            int retries = 0;
+            Anime anime = null;
+            do
+            {
+                anime = jikan.GetAnime(entryId).Result;
+                if (anime == null)
+                {
+                    Task.Delay(RetryWaitingTime);
+                    retries++;
+                }
+            } while (anime == null && retries < RetryMaxNumber);
+            return new JikanAnimeAdapter(anime);
+        }
+    }
+}
---
+++ AniCharades.API/Algorithms/SeriesAssembler/MangaSeriesAssembler.cs
@@ -0,0 +1,43 @@
+﻿using AniCharades.API.Adapters.Jikan;
+using JikanDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AniCharades.API.Algorithms.SeriesAssembler
+{
+    public class MangaSeriesAssembler : AbstractSeriesAssembler<JikanMangaAdapter>
+    {
+        private static readonly int RetryMaxNumber = 3;
+        private static readonly int RetryWaitingTime = 60 * 1000;
+
+        private readonly IJikan jikan;
+
+        public MangaSeriesAssembler(IJikan jikan)
+        {
+            this.jikan = jikan;
+        }
+
+        protected override bool CheckIfRelationIsValidForSeries(JikanMangaAdapter sourceEntry, JikanMangaAdapter relatedEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override JikanMangaAdapter GetEntry(long entryId)
+        {
+            int retries = 0;
+            Manga manga = null;
+            do
+            {
+                manga = jikan.GetManga(entryId).Result;
+                if (manga == null)
+                {
+                    Task.Delay(RetryWaitingTime);
+                    retries++;
+                }
+            } while (manga == null && retries < RetryMaxNumber);
+            return new JikanMangaAdapter(manga);
+        }
+    }
+}
