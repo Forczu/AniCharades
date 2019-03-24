708765d7671ab25b628d6cbb98246315d4aedb4e
Forczu <mateusz.forczmanski@gmail.com>
2019-03-24 19:07:24 +0100

    Created RelatedItem model for defining relations between entires

---
+++ AniCharades.Data/Enumerations/RelationType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AniCharades.Data.Enumerations
+{
+    public enum RelationType
+    {
+        None,
+        AlternativeSetting,
+        AlternativeVersion,
+        Adaptation,
+        Characters,
+        Other,
+        ParentStory,
+        Prequel,
+        Sequel,
+        SideStory,
+        SpinOff,
+        Summary
+    }
+}
---
+++ AniCharades.Data/Models/RelatedItem.cs
@@ -0,0 +1,17 @@
+﻿using AniCharades.Data.Enumerations;
+
+namespace AniCharades.Data.Models
+{
+    public class RelatedItem
+    {
+        public long MalId { get; }
+
+        public RelationType RelationType { get; }
+
+        public RelatedItem(long malId, RelationType relationType)
+        {
+            MalId = malId;
+            RelationType = relationType;
+        }
+    }
+}
