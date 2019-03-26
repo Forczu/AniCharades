e4a1b5649a0a0a0a5adef8963c7610676bd9f533
Forczu <mateusz.forczmanski@gmail.com>
2019-03-26 22:26:43 +0100

    Added a test for Nyaruko franchise relations

---
+++ AniCharades.API.Tests/Relations/WhenSeriesAssemblerWorksCorrectly.cs
@@ -0,0 +1,40 @@
+﻿using AniCharades.API.Algorithms.SeriesAssembler;
+using AniCharades.API.Tests.LargeMocks;
+using AniCharades.Data.Context;
+using AniCharades.Data.Enumerations;
+using AniCharades.Data.Models;
+using AniCharades.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AniCharades.API.Tests.Relations
+{
+    public class WhenSeriesAssemblerWorksCorrectly
+    {
+        private readonly AnimeSeriesAssembler animeSeriesAssembler;
+
+        public WhenSeriesAssemblerWorksCorrectly()
+        {
+            var repoMock = new Mock<IRelationCriteriaRepository>();
+            repoMock.SetReturnsDefault(Task.FromResult(new RelationCriteria { Strategy = "nyaruko" }));
+            var jikanMock = new JikanMockBuilder().HasAllAnimes().Build();
+            animeSeriesAssembler = new AnimeSeriesAssembler(jikanMock.Object, repoMock.Object);
+        }
+
+        [Fact]
+        public void NyarukoFranchiseShouldHaveAllTitlesFor2018()
+        {
+            // given
+            long nyarukoId = 11785;
+            // when
+            var nyarukoSeries = animeSeriesAssembler.Assembly(nyarukoId);
+            // 
+            Assert.Equal(8, nyarukoSeries.Count);
+        }
+    }
+}
