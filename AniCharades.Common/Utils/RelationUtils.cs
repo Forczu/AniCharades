c0716e719055c1c59ca6603b3e39bc93eaa63978
Forczu <mateusz.forczmanski@gmail.com>
2019-03-26 21:54:38 +0100

    Added most used string utils for comapring titles

---
+++ AniCharades.Common/Utils/RelationUtils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AniCharades.Common.Utils
+{
+    public static class RelationUtils
+    {
+        public static bool ContainsAnyKeyword(this string title, string[] keywords)
+        {
+            return keywords.Any(k => title.ToLower().Contains(k.ToLower()));
+        }
+
+        public static bool ContainsEveryKeyword(this string title, string[] keywords)
+        {
+            return keywords.All(k => title.ToLower().Contains(k.ToLower()));
+        }
+
+        public static bool IsSubtitleOf(this string firstTitle, string secondTitle)
+        {
+            return secondTitle.ToLower().Contains(firstTitle.ToLower());
+        }
+
+        public static bool ContainsAnySharedWord(this string firstTitle, string secondTitle)
+        {
+            var firstWords = firstTitle.Split(' ');
+            var secondWords = secondTitle.Split(' ');
+            bool hasSharedWord = firstWords.Any(fw => secondWords.Any(sw => WordsAreEqual(sw, fw)));
+            return hasSharedWord;
+        }
+
+        public static bool ContainsEverySharedWord(this string firstTitle, string secondTitle)
+        {
+            if (firstTitle.Count() < secondTitle.Count())
+                return ShorterTitleContainsEverySharedWord(firstTitle, secondTitle);
+            return ShorterTitleContainsEverySharedWord(secondTitle, firstTitle);
+        }
+
+        private static bool ShorterTitleContainsEverySharedWord(string shorterTitle, string longerTitle)
+        {
+            var shorterTitleWords = shorterTitle.Split(' ');
+            var longerTitleWords = longerTitle.Split(' ');
+            bool hasAllSharedWord = shorterTitleWords.All(stw => longerTitleWords.Any(ltw => WordsAreEqual(ltw, stw)));
+            return true;
+        }
+
+        private static bool WordsAreEqual(string firstWord, string secondWord)
+        {
+            return firstWord.RemoveSpecialCharacters().Equals(secondWord.RemoveSpecialCharacters());
+        }
+
+        public static string RemoveSpecialCharacters(this string title)
+        {
+            return Regex.Replace(title, @"[^\w\d\s]", string.Empty);
+        }
+    }
+}
