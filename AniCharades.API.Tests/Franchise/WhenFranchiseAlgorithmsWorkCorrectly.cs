96bdb5c1af2f2bd4e3b6f13a2b452e9a9162e94a
Forczu <mateusz.forczmanski@gmail.com>
2019-03-27 20:38:08 +0100

    Refactored franchise tests

--- AniCharades.API.Tests/Franchise/WhenFranchiseAlgorithmsWorkCorrectly.cs
+++ AniCharades.API.Tests/Franchise/WhenFranchiseAlgorithmsWorkCorrectly.cs
@@ -2,6 +2,7 @@
 using AniCharades.API.Algorithms.Franchise;
 using AniCharades.API.Tests.LargeMocks;
 using JikanDotNet;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -13,20 +14,34 @@ namespace AniCharades.API.Tests.Franchise
 {
     public class WhenFranchiseAlgorithmsWorkCorrectly
     {
-        private static readonly long[] NyarukoEntries = { 7596, 9598, 10477, 11785, 15699, 18713, 23619, 26023 };
-        private static readonly long[] LoveLiveEntries = { 9907, 9930, 11033, 12637, 14951, 15051, 19111, 20745,
-            20877, 21853, 24997, 25897, 30829, 30896, 31780, 32476, 32481, 32526, 32730, 32953, 33106, 34973, 35006, 36473, 37027 };
-        private readonly Mock<IJikan> jikanMock = new JikanMockBuilder()
-            .HasAnimes(NyarukoEntries)
-            .HasAnimes(LoveLiveEntries)
-            .Build();
+        private readonly Mock<IJikan> jikanMock;
+        private readonly IConfigurationRoot config;
+        private readonly FranchiseCreator franchiseCreator;
+
+        private readonly long[] nyarukoEntries;
+        private readonly long[] loveLiveeEntires;
+
+        public WhenFranchiseAlgorithmsWorkCorrectly()
+        {
+            var envVariable = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{envVariable}.json", optional: true)
+                    .Build();
+            franchiseCreator = new FranchiseCreator();
+            nyarukoEntries = config.GetSection("Jikan:Anime:Franchises:Nyaruko").Get<long[]>();
+            loveLiveeEntires = config.GetSection("Jikan:Anime:Franchises:LoveLive").Get<long[]>();
+            jikanMock = new JikanMockBuilder()
+                .HasAnimes(nyarukoEntries)
+                .HasAnimes(loveLiveeEntires)
+                .Build();
+        }
 
         [Fact]
         public void NyarukoSeriesShouldHaveFirstTvSeriesSelectedAsMain()
         {
             // given
-            var franchiseCreator = new FranchiseCreator();
-            var nyarukoAdapters = NyarukoEntries.Select(n => new JikanAnimeAdapter(jikanMock.Object.GetAnime(n).Result)).ToArray();
+            var nyarukoAdapters = nyarukoEntries.Select(n => new JikanAnimeAdapter(jikanMock.Object.GetAnime(n).Result)).ToArray();
             // when
             var nyarukoSeries = franchiseCreator.Create(nyarukoAdapters);
             // then
@@ -38,8 +53,7 @@ namespace AniCharades.API.Tests.Franchise
         public void LoveLiveSeriesShouldHaveFirstTvSeriesSelectedAsMain()
         {
             // given
-            var franchiseCreator = new FranchiseCreator();
-            var loveLiveAdapters = LoveLiveEntries.Select(n => new JikanAnimeAdapter(jikanMock.Object.GetAnime(n).Result)).ToArray();
+            var loveLiveAdapters = loveLiveeEntires.Select(n => new JikanAnimeAdapter(jikanMock.Object.GetAnime(n).Result)).ToArray();
             // when
             var loveLiveSeries = franchiseCreator.Create(loveLiveAdapters);
             // then
