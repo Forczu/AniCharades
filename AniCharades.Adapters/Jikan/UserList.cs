f76c697181dd91015468dc38880b6ca80cb70df6
Forczu <mateusz.forczmanski@gmail.com>
2019-04-08 15:48:29 +0200

    Created an adapter for user list

---
+++ AniCharades.Adapters/Jikan/UserList.cs
@@ -0,0 +1,34 @@
+﻿using AniCharades.Adapters.Interfaces;
+using JikanDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AniCharades.Adapters.Jikan
+{
+    public class UserList
+    {
+        public ICollection<IListEntry> Entries { get; set; }
+
+        public string Username { get; set; }
+
+        public UserList(ICollection<IListEntry> entries, string username)
+        {
+            Entries = entries;
+            Username = username;
+        }
+
+        public UserList(ICollection<AnimeListEntry> entries, string username)
+        {
+            Entries = entries.Select(e => new JikanAnimeListEntryAdapter(e)).Cast<IListEntry>().ToList();
+            Username = username;
+        }
+
+        public UserList(ICollection<MangaListEntry> entries, string username)
+        {
+            Entries = entries.Select(e => new JikanMangaListEntryAdapter(e)).Cast<IListEntry>().ToList();
+            Username = username;
+        }
+    }
+}
