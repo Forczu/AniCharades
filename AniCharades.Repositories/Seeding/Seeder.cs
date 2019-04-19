e16bef223e94dc6f95e3b396f70f07573353a0b9
Forczu <mateusz.forczmanski@gmail.com>
2019-04-19 23:19:36 +0200

    Fix wrong namespace in Seeder

--- AniCharades.Repositories/Seeding/Seeder.cs
+++ AniCharades.Repositories/Seeding/Seeder.cs
@@ -2,7 +2,7 @@
 using AniCharades.Repositories.Interfaces;
 using System.IO;
 
-namespace AniCharades.Common
+namespace AniCharades.Repositories.Seeding
 {
     public static class Seeder
     {
@@ -14,7 +14,7 @@ namespace AniCharades.Common
         public static void FillDatabaseFromJson(string filePath, ISeriesRepository seriesRepository)
         {
             var jsonFile = File.ReadAllText(filePath);
-            var seriesCollection = Serialization.JsonToCollection<SeriesEntry>(jsonFile);
+            var seriesCollection = Common.Serialization.JsonToCollection<SeriesEntry>(jsonFile);
             foreach (var series in seriesCollection)
             {
                 if (!seriesRepository.SeriesExists(series.Id).Result)
